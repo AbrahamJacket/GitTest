77f6662c4b6188245276c9f6818f4ce0a5cf0e1c
Abraham <chobanu.ilya@icloud.com>
2021-11-12 18:34:18 +0200

    Переписал sout

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("i");
+        }
     }
 }
